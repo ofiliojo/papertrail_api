21f0801ad04850f668613e7cc899e0626ae7a2de
Ofili Ichaba <ofiliojo@gmail.com>
2017-01-28 08:30:24 -0500

    login and logout sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :expenses
+  post    'sessions'     => 'sessions#create'
+  delete  'sessions/:id' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
